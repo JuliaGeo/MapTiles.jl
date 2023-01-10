1e019cb70f4efbfce179dc125cc7b81efbaea6e8
rafaqz <rafaelschouten@gmail.com>
2023-01-10 18:14:37 +0000

    init TileProviders

--- TileProviders.jl/test/runtests.jl
+++ TileProviders.jl/test/runtests.jl
@@ -2,5 +2,24 @@ using TileProviders
 using Test
 
 @testset "TileProviders.jl" begin
-    # Write your tests here.
+    providers = (
+        Leaflet.OSM(),
+        Leaflet.OSMFrance(),
+        Leaflet.OSMDE(),
+        Leaflet.OpenTopoMap(),
+        Leaflet.CARTO(:dark_nolabels),
+        Leaflet.Esri(),
+        Leaflet.Stamen(),
+        Leaflet.Stamen(:watercolor),
+        Leaflet.CARTO(:dark_all),
+        Leaflet.Google(:hybrid),
+        Leaflet.MapBox(; tileset_id="username.id", access_token="sometoken"),
+        Leaflet.Jawg(; access_token="sometoken"),
+        Leaflet.Thunderforest(; apikey="someapikey"),
+        Leaflet.OpenWeatherMap(:clouds; apikey="someapikey"),
+        Leaflet.OpenWeatherMap(:clouds; apikey="someapikey"),
+        Leaflet.NASAGIBS(:VIIRS_CityLights_2012),
+        Leaflet.NASAGIBS(:AMSRE_Brightness_Temp_89H_Day; date=Date(2010, 05, 07)),
+        Leaflet.Provider("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    )
 end
