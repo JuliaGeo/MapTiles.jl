5ceaef074803b7212cce9fa251311cc301ac567c
Simon <sdanisch@protonmail.com>
2023-01-16 21:11:50 +0000

    Fix package and make it work for Tyler.jl (#19)
    * make things work
    
    * remove using GeoInterface
    
    * clean up + introduce tile type
    
    * update TOMLs and gitignore
    
    * add new code based on mercantile
    
    * add project on extent and use it in TileGrid
    
    * fix method ambiguity and add tests
    
    * remove try.jl
    
    * use TileProviders.jl for tiles
    
    * exports
    
    * working with Tyler
    
    Co-authored-by: rafaqz <rafaelschouten@gmail.com>
    Co-authored-by: Martijn Visser <mgvisser@gmail.com>

--- src/MapTiles.jl
+++ src/MapTiles.jl
@@ -1,179 +1,19 @@
 module MapTiles
 
-    import Logging, Parameters, ProgressMeter
-    import HTTP, ImageMagick, ProtoBuf
-    import RecipesBase, GeoInterface
+import HTTP, ImageMagick, ProtoBuf
+using Base: @kwdef # exist since julia 1.1, exported only since 1.9
+using Extents
+using GeoInterface: GeoInterface, extent
+using GeoFormatTypes: GeoFormatTypes, CoordinateReferenceSystemFormat
 
-    # include("protobuf/2.1/vector_tile.jl")
+import TileProviders
 
-    mutable struct BaseMap{T}
-        img::Matrix{T}
-        zoom::Int
-        xmin::Int
-        xmax::Int
-        ymin::Int
-        ymax::Int
-        tilesize::Tuple{Int,Int}
-    end
+using TileProviders: geturl, AbstractProvider, Provider
 
-    RecipesBase.@recipe function f(basemap::BaseMap)
-        szx, szy = basemap.tilesize
-        xlims --> (0, szx * (1 + basemap.xmax - basemap.xmin))
-        ylims --> (0, szy * (1 + basemap.ymax - basemap.ymin))
-        ticks --> nothing
-        grid --> false
-        aspect_ratio --> 1
-        framestyle --> :none
-        basemap.img
-    end
+const Providers = TileProviders
 
-    include("providers.jl")
-    include("utils.jl")
+export Tile, TileGrid, geturl, Providers
 
-    """
-    Fetch map tiles composing a box at a given zoom level, and
-    return the assembled image.
-    """
-    function fetchmap(
-            minlon::Real, minlat::Real, maxlon::Real, maxlat::Real, z::Integer=18;
-            maxtiles::Int = 16,
-            provider::AbstractProvider = OpenStreetMapProvider(variant="standard")
-        )
-        xmin, ymin, xmax, ymax = tilebox(minlon, minlat, maxlon, maxlat, z)
-        xmin, ymin, xmax, ymax = correctbox(xmin, ymin, xmax, ymax, z)
-        sx, sy = boxsize(provider, xmin, ymin, xmax, ymax)
-        while sx * sy >= min(maxtiles, provider.maxtiles)
-            z -= 1
-            xmin, ymin, xmax, ymax = tilebox(minlon, minlat, maxlon, maxlat, z)
-            xmin, ymin, xmax, ymax = correctbox(xmin, ymin, xmax, ymax, z)
-            sx, sy = boxsize(provider, xmin, ymin, xmax, ymax)
-        end
-        Logging.@info(string("Setting zoom to ", z))
-        Logging.@info(string("Converting bounding latlon to tiles: ",
-            xmin, ",", ymin, ",", xmax, ",", ymax, " (xmin,ymin,xmax,ymax)"
-        ))
-        fetchtiles(xmin, ymin, xmax, ymax, z, maxtiles=maxtiles, provider=provider)
-    end
-
-    function fetchtiles(
-            xmin::Integer, ymin::Integer, xmax::Integer, ymax::Integer, z::Integer;
-            maxtiles::Int = 16,
-            provider::AbstractProvider = OpenStreetMapProvider(variant="standard")
-        )
-        sx, sy = boxsize(provider, xmin, ymin, xmax, ymax)
-        tmptile = fetchrastertile(provider, xmin, ymin, z) # fetch a tile for metadata
-        T = eltype(tmptile)
-        tilesizey, tilesizex = size(tmptile)
-        Logging.@info(string("Tiles: Matrix{",T,"}(", tilesizey, ",", tilesizex, ")"))
-        img = Matrix{T}(undef, sy*tilesizey, sx*tilesizex)
-        Logging.@info(string("Requesting ", sx, " x ", sy, " tiles"))
-        ProgressMeter.@showprogress for x in xmin:xmax, y in ymin:ymax
-            px = tilesizex * (x - xmin); py = tilesizey * (y - ymin)
-            img[py .+ (1:tilesizey), px .+ (1:tilesizex)] =
-                fetchrastertile(provider, x, y, z)
-        end
-        BaseMap(img, z,
-            min(xmin, xmax), max(xmin, xmax),
-            min(ymin, ymax), max(ymin, ymax),
-            (tilesizey, tilesizex)
-        )
-    end
-
-    function project(
-            ::Type{GeoInterface.Point},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        lon,lat = GeoInterface.coordinates(geom)
-        GeoInterface.Point(collect(lonlat2tile(basemap,lon,lat)))
-    end
-
-    function project(
-            ::Type{GeoInterface.MultiPoint},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        GeoInterface.MultiPoint([
-            collect(lonlat2tile(basemap,lon,lat))
-            for (lon,lat) in GeoInterface.coordinates(geom)
-        ])
-    end
-
-    function project(
-            ::Type{GeoInterface.LineString},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        GeoInterface.LineString([
-            collect(lonlat2tile(basemap,lon,lat))
-            for (lon,lat) in GeoInterface.coordinates(geom)
-        ])
-    end
-
-    function project(
-            ::Type{GeoInterface.MultiLineString},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        GeoInterface.MultiLineString([
-            [collect(lonlat2tile(basemap,lon,lat)) for (lon,lat) in line]
-            for line in GeoInterface.coordinates(geom)
-        ])
-    end
-
-    function project(
-            ::Type{GeoInterface.Polygon},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        GeoInterface.Polygon([
-            [collect(lonlat2tile(basemap,lon,lat)) for (lon,lat) in ring]
-            for ring in GeoInterface.coordinates(geom)
-        ])
-    end
-
-    function project(
-            ::Type{GeoInterface.MultiPolygon},
-            basemap::BaseMap,
-            geom::GeoInterface.AbstractGeometry
-        )
-        GeoInterface.MultiPolygon([[
-                [collect(lonlat2tile(basemap,lon,lat)) for (lon,lat) in ring]
-                for ring in poly
-            ]
-            for poly in GeoInterface.coordinates(geom)
-        ])
-    end
-
-    project(basemap::BaseMap, geom::GeoInterface.AbstractPoint) = 
-        project(GeoInterface.Point, basemap, geom)
-    project(basemap::BaseMap, geom::GeoInterface.AbstractMultiPoint) = 
-        project(GeoInterface.MultiPoint, basemap, geom)
-    project(basemap::BaseMap, geom::GeoInterface.AbstractLineString) = 
-        project(GeoInterface.LineString, basemap, geom)
-    project(basemap::BaseMap, geom::GeoInterface.AbstractMultiLineString) = 
-        project(GeoInterface.MultiLineString, basemap, geom)
-    project(basemap::BaseMap, geom::GeoInterface.AbstractPolygon) = 
-        project(GeoInterface.Polygon, basemap, geom)
-    project(basemap::BaseMap, geom::GeoInterface.AbstractMultiPolygon) = 
-        project(GeoInterface.MultiPolygon, basemap, geom)
-
-    function project(basemap::BaseMap, geom::GeoInterface.AbstractGeometry)
-        gtype = GeoInterface.geotype(geom)
-        if gtype == :Point
-            return project(GeoInterface.Point, basemap, geom)
-        elseif gtype == :MultiPoint
-            return project(GeoInterface.MultiPoint, basemap, geom)
-        elseif gtype == :LineString
-            return project(GeoInterface.LineString, basemap, geom)
-        elseif gtype == :MultiLineString
-            return project(GeoInterface.MultiLineString, basemap, geom)
-        elseif gtype == :Polygon
-            return project(GeoInterface.Polygon, basemap, geom)
-        else
-            @assert gtype == :MultiPolygon
-            return project(GeoInterface.MultiPolygon, basemap, geom)
-        end
-    end
+include("tiles.jl")
 
 end
---
+++ src/tiles.jl
@@ -0,0 +1,247 @@
+"""
+Determine the number of meters per pixel
+
+### Parameters
+* `lat`: latitude in radians
+* `z`: zoom level
+
+Source: http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Resolution_and_Scale
+"""
+function resolution(lat::Real, z::Integer)
+    meter_per_pixel = 156543.03 # For zoom = 0 at equator
+    meter_per_pixel * cos(lat) / (2^z)
+end
+
+const R2D = 180 / pi
+const RE = 6378137.0
+const CE = 2 * pi * RE
+const EPSILON = 1e-14
+const LL_EPSILON = 1e-11
+
+# represent the two major CRS so we can dispatch on them
+struct WGS84 <: CoordinateReferenceSystemFormat end
+struct WebMercator <: CoordinateReferenceSystemFormat end
+
+Base.convert(::Type{GeoFormatTypes.EPSG}, ::WebMercator) = EPSG(3857)
+Base.convert(::Type{GeoFormatTypes.EPSG}, ::WGS84) = EPSG(4326)
+Base.convert(::Type{String}, ::WebMercator) = "EPSG:3857"
+Base.convert(::Type{String}, ::WGS84) = "EPSG:4326"
+Base.convert(::Type{Int}, ::WebMercator) = 3857
+Base.convert(::Type{Int}, ::WGS84) = 4326
+
+"Convert web mercator x, y to longitude and latitude"
+function project(point, from::WebMercator, to::WGS84)
+    x = GeoInterface.x(point)
+    y = GeoInterface.y(point)
+
+    lng = x * R2D / RE
+    lat = ((pi / 2) - 2.0 * atan(exp(-y / RE))) * R2D
+    return lng, lat
+end
+
+"Convert longitude and latitude to web mercator x, y"
+function project(point, from::WGS84, to::WebMercator)
+    lng = GeoInterface.x(point)
+    lat = GeoInterface.y(point)
+
+    x = RE * deg2rad(lng)
+
+    y = if lat <= -90
+        -Inf
+    elseif lat >= 90
+        Inf
+    else
+        RE * log(tan((pi / 4) + (0.5 * deg2rad(lat))))
+    end
+
+    return x, y
+end
+
+function project_extent(bbox::Extent, from::CoordinateReferenceSystemFormat, to::CoordinateReferenceSystemFormat)
+    left, bottom = project((bbox.X[1], bbox.Y[1]), from, to)
+    right, top = project((bbox.X[2], bbox.Y[2]), from, to)
+
+    return Extent(X=(left, right), Y=(bottom, top))
+end
+
+const web_mercator = WebMercator()
+const wgs84 = WGS84()
+
+struct Tile
+    x::Int
+    y::Int
+    z::Int
+end
+
+Tile(index::CartesianIndex{2}, zoom::Integer) = Tile(index[1], index[2], zoom)
+
+"Get the tile containing a longitude and latitude"
+function Tile(point, zoom::Integer, crs::WGS84)
+    lng = GeoInterface.x(point)
+    lat = GeoInterface.y(point)
+
+    x = lng / 360.0 + 0.5
+    sinlat = sin(deg2rad(lat))
+    y = 0.5 - 0.25 * log((1.0 + sinlat) / (1.0 - sinlat)) / pi
+
+    Z2 = 2^zoom
+
+    xtile = if x <= 0
+        0
+    elseif x >= 1
+        Z2 - 1
+    else
+        # To address loss of precision in round-tripping between tile
+        # and lng/lat, points within EPSILON of the right side of a tile
+        # are counted in the next tile over.
+        floor(Int, (x + EPSILON) * Z2)
+    end
+    ytile = if y <= 0
+        0
+    elseif y >= 1
+        Z2 - 1
+    else
+        floor(Int, (y + EPSILON) * Z2)
+    end
+    return Tile(xtile, ytile, zoom)
+end
+
+struct TileGrid
+    grid::CartesianIndices{2, Tuple{UnitRange{Int}, UnitRange{Int}}}
+    z::Int
+end
+
+TileGrid(tile::Tile) = TileGrid(CartesianIndices((tile.x:tile.x, tile.y:tile.y)), tile.z)
+
+"Get the tiles overlapped by a geographic bounding box"
+function TileGrid(bbox::Extent, zoom::Int, crs::WGS84)
+    # Mercantile splits the bbox in two along the antimeridian if this happens.
+    # Decide if that case should be handled here or before, also considering
+    # antimeridian discussion in https://github.com/rafaqz/Extents.jl/issues/4
+    @assert bbox.X[1] < bbox.X[2]
+
+    # Clamp bounding values.
+    max_bbox = Extent(X = (-180.0, 180.0), Y = (-85.051129, 85.051129))
+    bbox = Extents.intersect(bbox, max_bbox)
+
+    ul_tile = Tile((bbox.X[1], bbox.Y[1]), zoom, crs)
+    lr_tile = Tile((bbox.X[2] - LL_EPSILON, bbox.Y[2] + LL_EPSILON), zoom, crs)
+
+    grid = CartesianIndices((ul_tile.x:lr_tile.x, lr_tile.y:ul_tile.y))
+    return TileGrid(grid, zoom)
+end
+
+"Get the tiles overlapped by a web mercator bounding box"
+function TileGrid(bbox::Extent, zoom::Int, crs::WebMercator)
+    bbox = project_extent(bbox, crs, wgs84)
+    return TileGrid(bbox, zoom, wgs84)
+end
+
+Base.length(tilegrid::TileGrid) = length(tilegrid.grid)
+Base.size(tilegrid::TileGrid, dims...) = size(tilegrid.grid, dims...)
+Base.getindex(tilegrid::TileGrid, i) = Tile(tilegrid.grid[i], tilegrid.z)
+Base.firstindex(tilegrid::TileGrid) = firstindex(tilegrid.grid)
+Base.lastindex(tilegrid::TileGrid) = lastindex(tilegrid.grid)
+
+function Base.iterate(tilegrid::TileGrid, state=1)
+    if state > length(tilegrid)
+        nothing
+    else
+        (tilegrid[state], state+1)
+    end
+end
+
+"Returns the bounding box of a tile in lng lat"
+function GeoInterface.extent(tile::Tile, crs::WGS84)
+    Z2 = 2^tile.z
+
+    ul_lon_deg = tile.x / Z2 * 360.0 - 180.0
+    ul_lat_rad = atan(sinh(pi * (1 - 2 * tile.y / Z2)))
+    ul_lat_deg = rad2deg(ul_lat_rad)
+
+    lr_lon_deg = (tile.x + 1) / Z2 * 360.0 - 180.0
+    lr_lat_rad = atan(sinh(pi * (1 - 2 * (tile.y + 1) / Z2)))
+    lr_lat_deg = rad2deg(lr_lat_rad)
+
+    return Extent(X = (ul_lon_deg, lr_lon_deg), Y = (lr_lat_deg, ul_lat_deg))
+end
+
+"Get the web mercator bounding box of a tile"
+function GeoInterface.extent(tile::Tile, crs::WebMercator)
+    tile_size = CE / 2^tile.z
+
+    left = tile.x * tile_size - CE / 2
+    right = left + tile_size
+
+    top = CE / 2 - tile.y * tile_size
+    bottom = top - tile_size
+
+    return Extent(X=(left, right), Y=(bottom, top))
+end
+
+"Returns the bounding box of a tile in lng lat"
+function GeoInterface.extent(tilegrid::TileGrid, crs::WGS84)
+    Z2 = 2^tilegrid.z
+
+    ul_idx = tilegrid.grid[begin]
+    lr_idx = tilegrid.grid[end]
+    ul_x, ul_y = ul_idx[1], ul_idx[2]
+    lr_x, lr_y = lr_idx[1], lr_idx[2]
+
+    ul_lon_deg = ul_x / Z2 * 360.0 - 180.0
+    ul_lat_rad = atan(sinh(pi * (1 - 2 * ul_y / Z2)))
+    ul_lat_deg = rad2deg(ul_lat_rad)
+
+    lr_lon_deg = (lr_x + 1) / Z2 * 360.0 - 180.0
+    lr_lat_rad = atan(sinh(pi * (1 - 2 * (lr_y + 1) / Z2)))
+    lr_lat_deg = rad2deg(lr_lat_rad)
+
+    return Extent(X = (ul_lon_deg, lr_lon_deg), Y = (lr_lat_deg, ul_lat_deg))
+end
+
+"Get the web mercator bounding box of a tile"
+function GeoInterface.extent(tilegrid::TileGrid, crs::WebMercator)
+    tile_size = CE / 2^tilegrid.z
+
+    ul_idx = tilegrid.grid[begin]
+    ul_x, ul_y = ul_idx[1], ul_idx[2]
+    nx, ny = size(tilegrid)
+
+    left = ul_x * tile_size - CE / 2
+    right = left + tile_size * nx
+
+    top = CE / 2 - ul_y * tile_size
+    bottom = top - tile_size * ny
+
+    return Extent(X=(left, right), Y=(bottom, top))
+end
+
+function TileProviders.geturl(provider::AbstractProvider, tile::Tile)
+    TileProviders.geturl(provider, tile.x, tile.y, tile.z)
+end
+
+function request(
+        provider::AbstractProvider,
+        tile::Tile,
+    )
+    url = geturl(provider,tile)
+    result = HTTP.get(url)
+    result.body
+end
+
+function fetchrastertile(
+        provider::AbstractProvider,
+        tile::Tile,
+    )
+    data = request(provider, tile)
+    ImageMagick.readblob(data)
+end
+
+function fetchvectortile(
+        provider::AbstractProvider,
+        tile::Tile
+    )
+    data = request(provider, tile)
+    ProtoBuf.readproto(IOBuffer(data), MapTiles.vector_tile.Tile())
+end
+
