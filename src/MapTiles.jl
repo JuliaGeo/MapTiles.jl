01696e50817aadbd73943468cf74fc1c0ab92ff2
SimonDanisch <sdanisch@protonmail.com>
2023-01-17 16:04:34 +0100

    prepare release

--- src/MapTiles.jl
+++ src/MapTiles.jl
@@ -1,11 +1,10 @@
 module MapTiles
 
-import HTTP, ImageMagick, ProtoBuf
+import HTTP, ImageMagick
 using Base: @kwdef # exist since julia 1.1, exported only since 1.9
 using Extents
 using GeoInterface: GeoInterface, extent
 using GeoFormatTypes: GeoFormatTypes, CoordinateReferenceSystemFormat
-
 import TileProviders
 
 using TileProviders: geturl, AbstractProvider, Provider
